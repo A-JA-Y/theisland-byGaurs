78d0b6d2bf66a7b77a29a65366c6611e674e9999
Ajay <mailtoajay7007@gmail.com>
2026-08-04 09:28:28 +0530

    api

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = "https://backend.daamrideals.com/api";
+const BASEURL = "https://api.jaypeegreen.com/api";
 // const BASEURL = "http://127.0.0.1:5000/api";
 
 const API = axios.create({
